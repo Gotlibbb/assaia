25b4db0f3fd0cc7632133dc2fee75fce29111292
Personal <bugs@ardentsky.com>
2023-01-22 19:58:02 +0300

    assaia-test

---
+++ src/app/App.styled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  width: 100%;
+  
+  h1 {
+    font-size: 35px;
+    align-self: center;
+  }
+`
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppContainer } from "./App.styled";
+import { Board } from "../features/board/Board";
+
+export const App = () => {
+  return (
+    <AppContainer>
+      <h1>Крестики нолики</h1>
+      <Board/>
+    </AppContainer>
+  );
+}
+
